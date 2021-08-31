e587fd3f5742b2a92d4eb70cd8b3ff835996b1d9
Carter McKaughan <cartermckaughan@Carters-MacBook-Pro.local>
2021-08-31 09:56:25 -0700

    Initial Commit

--- scrabble_scorer.py
+++ scrabble_scorer.py
@@ -17,9 +17,9 @@ def old_scrabble_scorer(word):
 
     for char in word:
 
-        for point_value in old_point_structure:
+        for point_value in OLD_POINT_STRUCTURE:
 
-            if char in old_point_structure[point_value]:
+            if char in OLD_POINT_STRUCTURE[point_value]:
                 letterPoints += 'Points for {char}: {point_value}\n'.format(char = char, point_value = point_value)
 
     return letterPoints
@@ -29,24 +29,142 @@ def old_scrabble_scorer(word):
 
 def initial_prompt():
    print("Let's play some Scrabble!\n")
+   print("Enter a word to score: ")
+   user_input = input()
 
-
-def simple_scorer():
-   return 
-
-def vowel_bonus_scorer():
-    return 
-
-def scrabble_scorer():
-    return
-
-scoring_algorithms = ()
-
-def scorer_prompt():
-    return 
-
-def transform():
-    return
+   return user_input
 
 def run_program():
     word = initial_prompt()
+
+    older_score = old_scrabble_scorer(word)
+
+    print(older_score)
+    # Outputs:
+
+
+def simple_scorer(word):
+    #score = 0
+    #for letter in word.lower():
+        #score += 1
+        
+   return len(word)
+
+simple_score = simple_scorer('wizard')
+
+print(simple_score)
+# Outputs:
+# 6
+
+def vowel_bonus_scorer(word):
+    vowels = 'aeiou'
+    score = 0
+
+    for letter in word.lower():
+        point = 3 if letter in vowels else 1
+        score += point
+        
+    return score
+
+vowel_score = vowel_bonus_scorer('wizard')
+
+print(vowel_score)
+
+# Outputs:
+# 10
+
+def scrabble_scorer(word):
+    score = 0
+    
+    for char in word.lower():
+        score += new_point_structure[char.lower()]
+    
+    return score
+
+# scoring_algorithms = (
+#     {
+#         "name": "Simple Score",
+#         "description": "One point per character",
+#         "orer_function": simple_scorer
+#     },
+#     {
+#         "name": "Vowel Bonus",
+#         "description": "Vowels are worth 3 points",
+#         "scorer_function": vowel_bonus_scorer
+#     },
+#     {
+#         "name": "Scrabble",
+#         "description": "Uses scrabble point system",
+#         "scorer_function": scrabble_scorer
+#     }
+# )
+# #Outputs:
+
+def scorer_prompt():
+    print('Which scoring algorithm would you like to use?')
+
+
+    for index, algorithm in enumerate(scoring_algorithms):
+        print(f'{index} - {algorithm["name"]}: {algorithm["description"]}')
+
+        user_selection = int(input('Enter 0, 1, or 2:'))
+
+    selected_score_algorithm_dict = scoring_algorithms[user_selection]
+
+    return selected_score_algorithm_dict
+
+def transform(provided_dict):
+    new_dict = {}
+
+    new_dict["' '"] = 0
+
+    for (key, value) in provided_dict.items():
+        for letter in value:
+            new_dict[letter.lower()] = key
+
+    return new_dict
+
+
+new_point_structure = transform(OLD_POINT_STRUCTURE)
+
+simple_scorer_dict = {
+    'name': 'Simple',
+    'description': 'scores each letter provided is worth 1 point.',
+    'score_function': simple_scorer
+}
+
+vowel_bonus_scorer_dict = {
+    'name': 'Bonus Vowels',
+    'description': 'gives 3 point per vowels and 1 point per consonants',
+    'score_function': vowel_bonus_scorer
+}
+
+old_scrabble_scorer_dict = {
+    'name': 'Scrabble',
+    'description': 'uses the original Scrabble game point system',
+    'score_function': scrabble_scorer
+}
+
+scoring_algorithms = (
+    simple_scorer_dict,
+    vowel_bonus_scorer_dict,
+    old_scrabble_scorer_dict
+)
+
+
+def run_program():
+    word = initial_prompt()
+
+    selected_score_algorithm_dict = scorer_prompt()
+
+    score = selected_score_algorithm_dict['score_function'](word)
+
+    print(
+        f'''
+The word you entered was "{word}".
+You selected the "{selected_score_algorithm_dict["name"]}" scoring algorithm which {selected_score_algorithm_dict["description"]}.
+Your word is worth {score} points!'''
+    )
+
+
+run_program()
